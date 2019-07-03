c40c332265589c18354971cf63292bc663e848fe
caolinjing <764775185@qq.com>
2019-07-03 14:30:50 +0800

    Revert "[clj] add flyway_user"
    This reverts commit e9c78f184ea1d0006869b2fbfbe848ae8411b3b5.

--- build/resources/main/db/migration/V1.0__CreateUserTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(20) NOT NULL,
-  age INT NOT NULL,
-  gender VARCHAR(20) NOT NULL,
-  CONSTRAINT pk_user PRIMARY KEY (id),
-  CONSTRAINT pk_username UNIQUE (username)
-);
-
